31cdbea0f59ba4bf9d5396db887bc3505ebb1fef
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-17 04:18:03 +0300

    "second"

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+typedef struct pformat
+{
+    char symbol;
+    int (*print)(va_list varg);
+} prtmt;
+
+int _printf(const char *format, ...);
+int print_char(va_list varg);
+int print_str(va_list varg);
+int print_modular(va_list varg);
+
+
+#endif /*MAIN_H*/
