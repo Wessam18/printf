1abc7a64969b61421f2b9e88e2218d875e52b1be
Wessam Abdelnabi <138536543+Wessam18@users.noreply.github.com>
2023-09-17 14:30:48 +0300

    create test dir

--- main.h
+++ main.h
@@ -1,24 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+/********Headers_Declarations******/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 #include <limits.h>
 
+/***********Struct_Declarations**********/
+/**
+ * pformt: Structure name.
+ * @symbol: Input symbol
+ * print - function pointer
+ */
 typedef struct pformat
 {
     char symbol;
     int (*print)(va_list varg);
 } prtmt;
 
+/*******Function_Declarations************/
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list varg);
 int print_str(va_list varg);
 int print_modular(va_list varg);
-
 
 #endif /*MAIN_H*/
