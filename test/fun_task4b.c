115e2a79b7679f21a228924c855ae37805aaeb74
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-20 00:38:13 +0300

    "update task 3 hexadecimal"

--- test/fun_task4b.c
+++ test/fun_task4b.c
@@ -8,43 +8,27 @@
 int phexs(va_list varg)
 {
 	unsigned long int number = va_arg(varg, unsigned long int);
+int len = 0;
 
-	if (number > 0)
-	{
-		return (hexs_recur(number));
-	}
-	else
-	{
-		_putchar('0');
-		return (1);
-	}
-}
-/**
- * hexs_recur - a function to print hexadecimal
- * @value: input number to check.
- * Return: (length).
- */
-int hexs_recur(unsigned long int value)
-{
-	int len = 0;
-
-	if (value == 0)
+	if (number == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		if ((value % 16) <= 9)
+		if ((number % 16) <= 9)
 		{
-			len = (1 + hexs_recur(value / 16));
-			_putchar((value % 16) + '0');
+			len = (1 + (number / 16));
+			_putchar((number % 16) + '0');
 		}
 		else
 		{
-
-			len = (1 + hexs_recur(value / 16));
-			_putchar((value % 16) + 87);
+			len = (1 + (number / 16));
+			_putchar((number % 16) + 87);
 		}
 	}
 	return (len);
 }
+
+
+	
