d2ae289fdc6758c8ba26f4869f13aeb391b18ce4
wessam18 <wessamabdelnabi903@gmail.com>
2023-09-20 04:11:29 +0300

    "task3"

--- fun_task2.c
+++ fun_task2.c
@@ -9,27 +9,33 @@ int pbin(va_list varg)
 {
 	unsigned int number = va_arg(varg, unsigned int);
 
-	return (bin_recur(number));
-}
-/**
- * bin_recur - a function to print binary
- * @x: input number to check.
- * Return: (length).
- */
-int bin_recur(unsigned int x)
-{
-	int len = 0;
-
-	if (x / 10 == 0)
+	if (number > 0)
 	{
-		
-		len += _putchar((x % 10) + '0');
-
+		return (bin_recur(number));
 	}
 	else
 	{
-		len += bin_recur(x / 10);
-		len += _putchar((x % 10) + '0');
+		_putchar('0');
+		return (1);
+	}
+}
+/**
+ * bin_recur - a function to print binary
+ * @value: input number to check.
+ * Return: (length).
+ */
+int bin_recur(unsigned int value)
+{
+	int len = 0;
+
+	if (value == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		len = (1 + bin_recur(value / 2));
+		_putchar((value % 2) + '0');
 	}
 	return (len);
 }
